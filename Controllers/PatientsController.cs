1b493e87847f9bd32156c70706330eb95441c6ba
keithfeb14 <111192162+keithfeb14@users.noreply.github.com>
2024-03-19 22:05:09 +0530

    basic fucntionality working (no authorisation)

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VetDBM.Data;
 using VetDBM.Models;
@@ -77,6 +78,7 @@ namespace VetDBM.Controllers
 
             return View(patient);
         }
+
 
         // GET: Patients/Create
         public IActionResult Create()
